4b26dd4832ab75f48586f45cc70cec833970e69e
André Fernandes <andrefersouza@gmail.com>
2023-01-04 17:00:53 -0300

    update projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -10,23 +10,22 @@ export function Projects() {
 
     return (
             <div id='projects' className="relative w-full h-full flex flex-col-reverse justify-center items-center bg-black sm:px-20 lg:px-2 lg:left-1/2 lg:translate-x-[-50%]">
-                <img src={SkillsBg} alt="" className="hidden rotate-180 lg:block h-full w-full left-0"/>
                 <div className="max-w-7xl flex relative flex-col h-full w-full py-5 bg-black bg-opacity-40 border-x-2 border-zinc-900 border-opacity-80 backdrop-blur-sm pt-28 lg:pt-32">
 
                 <p className="flex mx-5 sm:mx-16 text-4xl border-b-2 w-56 border-green-500 pb-3 z-10">Projetos</p>
 
-                <div className="flex-row flex-wrap relative grid lg:grid-cols-2 xl:grid-cols-3 w-full content-around justify-items-center h-full py-6">
-                <a target='_blank' href='https://dredeco.github.io/Projeto_HC' className='flex py-3 justify-center'>
+                <div className="grid lg:grid-cols-2 xl:grid-cols-3 w-full h-auto content-around justify-items-center py-6">
+                <a target='_blank' href='https://dredeco.github.io/Projeto_HC' className='flex py-6 justify-center'>
                         <div className='flex items-center flex-col mx-5 lg:w-[80%] justify-center rounded-md bg-green-600 bg-opacity-40 duration-500 sm:hover:scale-105 hover:bg-opacity-100 hover:bg-green-900 lg:hover:scale-125 hover:z-50'>
                             <img src={ProjetoHC} className="relative h-full" />
                             <p className='flex flex-col justify-center w-full text-center p-2'>
                                 <h4 className='font-bold'>Coins</h4>
                                 Criada utilizando ReactJS, JavaScript, Firebase e SASS
-                            </p> 
+                            </p>
                         </div>
                     </a>
                     
-                    <a target='_blank' href='https://dredeco.github.io/Projeto-sass/' className='flex py-3 justify-center'>
+                    <a target='_blank' href='https://dredeco.github.io/Projeto-sass/' className='flex py-6 justify-center'>
                         <div className='flex items-center flex-col mx-5 lg:w-[80%] justify-center rounded-md bg-green-600 bg-opacity-40 duration-500 sm:hover:scale-105 hover:bg-opacity-100 hover:bg-green-900 lg:hover:scale-125 hover:z-50'>
                             <img src={ProjetoSass} className="relative h-full" />
                             <p className='flex flex-col justify-center w-full min-h-[30%] text-center py-2'>
@@ -36,7 +35,7 @@ export function Projects() {
                         </div>
                     </a>
 
-                    <a target='_blank' href='https://dredeco.github.io/calculadora/' className='flex py-3 justify-center'>
+                    <a target='_blank' href='https://dredeco.github.io/calculadora/' className='flex py-6 justify-center'>
                         <div className='flex items-center flex-col mx-5 lg:w-[80%] justify-center rounded-md bg-green-600 bg-opacity-40 duration-500 sm:hover:scale-105 hover:bg-opacity-100 hover:bg-green-900 lg:hover:scale-125 hover:z-50'>
                             <img src={Projeto2} className="relative h-full" />
                             <p className='flex flex-col justify-center w-full min-h-[30%] text-center py-2'>
@@ -46,7 +45,7 @@ export function Projects() {
                         </div>
                     </a>
 
-                    <a target='_blank' href='https://dredeco.github.io/Netflix-Clone/' className='flex py-3 justify-center'>
+                    <a target='_blank' href='https://dredeco.github.io/Netflix-Clone/' className='flex py-6 justify-center'>
                         <div className='flex items-center flex-col mx-5 lg:w-[80%] justify-center rounded-md bg-green-600 bg-opacity-40 duration-500 sm:hover:scale-105 hover:bg-opacity-100 hover:bg-green-900 lg:hover:scale-125 hover:z-50'>
                             <img src={NetflixClone} className="relative w-auto" />
                             <p className='relative w-full text-center py-2'>
